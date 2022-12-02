21d90d30b6e9b5e71454bdcb3cea32ec6f166146
Kei Yoshimura <yoshimura@prioris.jp>
2022-12-02 18:38:54 +0900

    Python3 & beautifulsoup4 support

--- eqxml2geojson.py
+++ eqxml2geojson.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
 import sys, re
 import csv, json
 import sqlite3
-from BeautifulSoup import BeautifulStoneSoup
+from bs4 import BeautifulSoup
 
 argvs = sys.argv
 argc = len(argvs)
 
 if (argc != 4):
-    print 'Usage: # python %s [centroid csvs dir] [input file(uuid.xml)] [output dir] ' % argvs[0]
+    print('Usage: # python %s [centroid csvs dir] [input file(uuid.xml)] [output dir] ' % argvs[0])
     quit()
 
 # JMAXMLのオープン
 xmlFile = open(argvs[2],'r')
-xml = BeautifulStoneSoup(xmlFile)
+xml = BeautifulSoup(xmlFile, "xml")
 
 csvDir = argvs[1]
 outputDir = argvs[3]
@@ -24,22 +23,22 @@ cursor = connection.cursor()
 
 # 細分区域レベルのテーブルを生成
 cursor.execute("CREATE TABLE jma_area_centroid (jma_code INTEGER PRIMARY KEY, jma_name TEXT, lat REAL, lon REAL);")
-with open(csvDir + "jma_area_centroid.csv",'rb') as fin:
+with open(csvDir + "jma_area_centroid.csv",'r') as fin:
     dr = csv.DictReader(fin, fieldnames = ('jma_code','jma_name','lat','lon'))
-    to_db = [(c['jma_code'], unicode(c['jma_name'], "utf8"), c['lat'], c['lon']) for c in dr]
+    to_db = [(c['jma_code'], c['jma_name'], c['lat'], c['lon']) for c in dr]
 cursor.executemany("INSERT INTO jma_area_centroid (jma_code, jma_name, lat, lon) VALUES (?, ?, ?, ?);", to_db)
 connection.commit()
 
 # 市区町村レベルのテーブルを生成
 cursor.execute("CREATE TABLE jma_city_centroid (jma_code INTEGER PRIMARY KEY, jma_name TEXT, lat REAL, lon REAL);")
-with open(csvDir + "jma_city_centroid.csv",'rb') as fin:
+with open(csvDir + "jma_city_centroid.csv",'r') as fin:
     dr = csv.DictReader(fin, fieldnames = ('jma_code','jma_name','lat','lon'))
-    to_db = [(c['jma_code'], unicode(c['jma_name'], "utf8"), c['lat'], c['lon']) for c in dr]
+    to_db = [(c['jma_code'], c['jma_name'], c['lat'], c['lon']) for c in dr]
 cursor.executemany("INSERT INTO jma_city_centroid (jma_code, jma_name, lat, lon) VALUES (?, ?, ?, ?);", to_db)
 connection.commit()
 
 # 震源の座標を取得
-epicenterStr = xml.report.body.earthquake.hypocenter.area.find('jmx_eb:coordinate').string
+epicenterStr = xml.Report.Body.Earthquake.Hypocenter.Area.find('jmx_eb:Coordinate').string
 # 分割
 epicenterCoord = re.split(r'(\-|\+|\/)', epicenterStr)
 # epicenterに入れる
@@ -59,28 +58,28 @@ cityLevelFeatures = [epicenter]
 areaLevelPoints = [[] for i in range(0,9)]
 cityLevelPoints = [[] for i in range(0,9)]
 
-if xml.report.body.find('intensity'):
+if xml.Report.Body.find('Intensity'):
 
     # 各震度ごとにpointsにまとめる
-    pref = xml.report.body.intensity.observation.findAll('pref')
+    pref = xml.Report.Body.Intensity.Observation.findAll('Pref')
     for p in pref:
-        area = p.findAll('area')
+        area = p.findAll('Area')
         for a in area:
-            cursor.execute("SELECT lat,lon,jma_name FROM jma_area_centroid WHERE jma_code=" + a.code.string)
+            cursor.execute("SELECT lat,lon,jma_name FROM jma_area_centroid WHERE jma_code=" + a.Code.string)
             row = cursor.fetchone()
-            if a.maxint is not None:
-                maxint = a.maxint.string
+            if a.MaxInt is not None:
+                maxint = a.MaxInt.string
             else:
-                maxint = ""
+                maxint = "5?" # 震度５弱以上未入電
             areaLevelFeatures.append({"type":"Feature","properties":{"class": maxint, "name": row[2]},"geometry":{"type":"Point","coordinates": [row[1],row[0]]}})
-            city = a.findAll('city')
+            city = a.findAll('City')
             for c in city:
-                cursor.execute("SELECT lat,lon,jma_name FROM jma_city_centroid WHERE jma_code=" + c.code.string)
+                cursor.execute("SELECT lat,lon,jma_name FROM jma_city_centroid WHERE jma_code=" + c.Code.string)
                 row = cursor.fetchone()
-                if c.maxint is not None:
-                    maxint = c.maxint.string
+                if c.MaxInt is not None:
+                    maxint = c.MaxInt.string
                 else:
-                    maxint = ""
+                    maxint = "5?" # 震度５弱以上未入電
                 cityLevelFeatures.append({"type":"Feature","properties":{"class": maxint, "name": row[2]},"geometry":{"type":"Point","coordinates": [row[1],row[0]]}})
 
 # featuresをfeatureCollectionに追加
@@ -89,11 +88,11 @@ cityLevelFeatureCollection = {"type":"FeatureCollection","features":cityLevelFea
 
 # jsonを出力
 f = open(outputDir+'smallScalePoints.json', 'w')
-f.write(json.dumps(areaLevelFeatureCollection))
+f.write(json.dumps(areaLevelFeatureCollection, ensure_ascii=False))
 f.close()
 
 f = open(outputDir+'largeScalePoints.json', 'w')
-f.write(json.dumps(cityLevelFeatureCollection))
+f.write(json.dumps(cityLevelFeatureCollection, ensure_ascii=False))
 f.close()
 
 # 後処理
